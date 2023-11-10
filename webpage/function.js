d906b767f016fb8f7f9f43a52bb651d4dbc37fbb
ssurampally <sreekanth.surampally@gmail.com>
2023-11-09 20:50:20 -0800

    functions

---
+++ webpage/function.js
@@ -0,0 +1,9 @@
+//calling a function and passing the result to a variable
+let multiply = myFunction(4, 3);
+
+console.log(multiply) ;
+
+//defining a function
+function myFunction(a, b) {
+  return a * b;
+}
